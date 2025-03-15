1f8283829d53d51f54ab4a3b8418c3677bccf4ff
febo <febo@anza.xyz>
2025-03-15 01:56:53 +0000

    Use nostd panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,5 @@
 
 mod entrypoint;
 
-// No allocator needed.
 pinocchio::no_allocator!();
-
-/// A panic handler for `no_std`.
-///
-/// TODO: Upstream this to `pinocchio` crate.
-#[cfg(not(test))]
-#[no_mangle]
-#[panic_handler]
-fn custom_panic(info: &core::panic::PanicInfo<'_>) -> ! {
-    if let Some(location) = info.location() {
-        pinocchio::log::sol_log(location.file());
-    }
-    // Panic reporting.
-    pinocchio::log::sol_log("** PANICKED **");
-    // Never returns.
-    loop {}
-}
+pinocchio::nostd_panic_handler!();
