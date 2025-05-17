a193060e7a611c4cd4dba05784d6b79e7c1f4ffe
febo <febo@anza.xyz>
2025-05-17 01:03:53 +0100

    Simplify processor

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -12,41 +12,16 @@ use {
 lazy_program_entrypoint!(process_instruction);
 
 pub fn process_instruction(mut context: InstructionContext) -> ProgramResult {
-    let mut output = [0u8; 44];
-    let mut missing_required_signature = false;
-
     // Process signer accounts (if any).
     while context.remaining() > 0 {
         // Duplicated accounts are implicitly checked since at least one of the
         // "copies" must be a signer.
         if let MaybeAccount::Account(account) = context.next_account()? {
-            if account.is_signer() {
-                let len = five8::encode_32(account.key(), &mut output);
-                // SAFETY: b58 encoding produces a valid UTF-8 string.
-                let as_str = unsafe { from_utf8_unchecked(output.get_unchecked(..len as usize)) };
-                log!("Signed by {}", as_str);
-            } else {
-                missing_required_signature = true;
+            if !account.is_signer() {
+                return Err(ProgramError::MissingRequiredSignature);
             }
         }
     }
-
-    if missing_required_signature {
-        return Err(ProgramError::MissingRequiredSignature);
-    }
-
-    // SAFETY: All accounts have been processed.
-    let instruction_data = unsafe { context.instruction_data_unchecked() };
-
-    log!(
-        1300,
-        "Memo (len {}): \"{}\"",
-        instruction_data.len(),
-        from_utf8(instruction_data).map_err(|error| {
-            log!(1300, "Invalid UTF-8, from byte {}", error.valid_up_to());
-            ProgramError::InvalidInstructionData
-        })?
-    );
 
     Ok(())
 }
